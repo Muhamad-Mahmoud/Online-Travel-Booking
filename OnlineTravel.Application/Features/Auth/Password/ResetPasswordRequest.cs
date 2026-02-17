ff1718670a3fc19a82c891e3b35d0db477156bc0
momen khaled <momenkhaled360@gmail.com>
2026-02-17 07:19:08 +0200

    updated Auth

---
+++ OnlineTravel.Application/Features/Auth/Password/ResetPasswordRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineTravel.Application.Features.Auth.Password
+{
+    public class ResetPasswordRequest
+    {
+        public string Email { get; set; } = default!;
+        public string Token { get; set; } = default!;
+        public string NewPassword { get; set; } = default!;
+    }
+
+}
